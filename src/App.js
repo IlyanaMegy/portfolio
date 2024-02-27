588a01762c61f55b9cea4af0ff83ed07aa92d0bb
Ilyana Megy <ilyanamegy7@gmail.com>
2024-02-27 12:38:18 +0100

    :white_check_mark: created menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import Header from './components/header/Header';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
+
+      <main className="main">
+        
+        <Home />
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
